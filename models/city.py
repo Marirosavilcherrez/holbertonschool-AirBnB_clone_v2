1842ba4bf456f20d48f093f616da1799246b6a45
Marirosa <arqmvilcherrez@gmail.com>
2023-09-07 10:40:32 -0700

    Update City and State inherits from BaseModels and Base

--- models/city.py
+++ models/city.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, Integer, String, create_engine
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+    state_id = Column(String(60), nullable=None, foreign_key=True)
+    name = Column(String(128), nullable=None)
