5dfd6ee073722f54f3e28ef43037e44231acc28b
Marirosa <arqmvilcherrez@gmail.com>
2023-09-08 12:05:37 -0700

    Fix id error conexion between cities and states

--- models/base_model.py
+++ models/base_model.py
@@ -43,8 +43,8 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        cls_name = self.__class__.__name__
+        return '[{}] ({}) {}'.format(cls_name, self.id, self.to_dict())
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
@@ -55,13 +55,11 @@ class BaseModel:
     def to_dict(self):
         """Convert instance into dict format"""
         dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        for key, value in self.__dict__.items():
+            if key != '_sa_instance_state' and key != '__class__':
+                dictionary[key] = value
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if '_sa_instance_state' in dictionary.keys():
-            dictionary.pop('_sa_instance_state', None)
         return dictionary
         
     def delete(self):
--- models/state.py
+++ models/state.py
@@ -12,17 +12,18 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
 
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", cascade="all, delete", backref="state")
-    else:
-        name = ""
+    name = Column(String(128), nullable=False)
 
-    @property
-    def cities(self):
-        "return the list of instances from City"
-        list_cities = []
-        for k in models.storage.all('City').values():
-            if k.state_id == self.id:
-                list_cities.append(k)
-        return list_cities
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", cascade="all, delete", backref="state")
+
+    else:
+        @property
+        def cities(self):
+            "return the list of instances from City"
+            from models import storage
+            list_cities = []
+            for k in models.storage.all('City').values():
+                if k.state_id == self.id:
+                    list_cities.append(k)
+            return list_cities
