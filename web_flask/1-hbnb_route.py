0f091de31802eaa2f0a0ec364822b460f00f648f
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-09-12 17:18:09 -0500

    fix pycodestyle need one more blank line

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 def hello_route():
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hi_route():
     return "HBNB"
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
