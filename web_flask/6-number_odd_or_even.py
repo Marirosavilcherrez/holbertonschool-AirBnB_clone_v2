515d3cd8852c8d0eb402a50377a34c6926d4d610
Marirosa <arqmvilcherrez@gmail.com>
2023-09-12 18:12:00 -0700

    Fix pycodestyle errors

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -38,13 +38,15 @@ def number_route(n):
 def number_template_route(n):
     if isinstance(n, int):
         return render_template('5-number.html', number=n)
-    
+
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_route(n):
     if isinstance(n, int):
-        odd_or_even= "even" if n % 2 == 0 else "odd"
-        return render_template('6-number_odd_or_even.html', number=n, odd_even=odd_or_even)
-    
+        odd_or_even = "even" if n % 2 == 0 else "odd"
+        return render_template('6-number_odd_or_even.html', number=n,
+                               odd_even=odd_or_even)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
