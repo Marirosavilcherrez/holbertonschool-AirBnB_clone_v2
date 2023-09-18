60e9ba20264acdfe3d0e82849eb326f7a4abd92d
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-09-18 15:13:21 -0700

    Fix not module documentation

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -9,41 +9,48 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_route():
+    "Display hello in flask"
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hi_route():
+    "Display HBNB for flask"
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
     text = text.replace("_", " ")
+    "Display C and replace _ for space"
     return f'C {text}'
 
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_route(text="is_cool"):
+    "Display Python follow by text"
     text = text.replace("_", " ")
     return f'Python {text}'
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_route(n):
+    "Display n if is an integer"
     if isinstance(n, int):
         return f'{n} is a number'
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template_route(n):
+    "Display html file if n is an integer"
     if isinstance(n, int):
         return render_template('5-number.html', number=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_route(n):
+    "Display HTML file of state objects in DBstorage"
     if isinstance(n, int):
         odd_or_even = "even" if n % 2 == 0 else "odd"
         return render_template('6-number_odd_or_even.html', number=n,
@@ -59,7 +66,7 @@ def html_route():
 
 @app.route('/cities_by_states', strict_slashes=False)
 def html_route():
-    "Display a list of all State objects sorted by name"
+    "Display a list of all State and cities objects sorted by name"
     states = storage.all('States')
     return render_template('8-cities_by_states.html', states=states)
 
